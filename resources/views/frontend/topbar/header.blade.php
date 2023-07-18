4d4e70083e6fa88521ff563c3580506d58f293a0
afzal-swe <afzalbhola07@gmail.com>
2023-07-18 18:28:51 +0600

    Update top bar Cart and order count

--- resources/views/frontend/topbar/header.blade.php
+++ resources/views/frontend/topbar/header.blade.php
@@ -5,6 +5,13 @@
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class=""> </span>
           </button>
+
+          @php
+             $id = Auth::user()->id;
+            $cart_view = DB::table('carts')->where('user_id', '=', $id)->get();
+            $order = DB::table('orders')->where('user_id', '=', $id)->get();
+          @endphp
+
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav">
                 <li class="nav-item active">
@@ -27,10 +34,10 @@
                    <a class="nav-link" href="contact.html">Contact</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="{{ route('cart.view') }}">Cart</a>
+                   <a class="nav-link" href="{{ route('cart.view') }}">Cart [{{count($cart_view)}}]</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="{{ route('order_view') }}">Order</a>
+                   <a class="nav-link" href="{{ route('order_view') }}">Order [{{count($order)}}]</a>
                 </li>
                 <form class="form-inline">
                   <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
